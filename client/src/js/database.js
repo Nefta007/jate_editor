cd02396f7c8b80898828717ef1051dc6128fa1ae
Neftali <nperc001@ucr.edu>
2023-12-19 15:05:53 -0800

    changd typos and result presentation

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const jateDb = await openDB('contact', 1);
+  const jateDb = await openDB('jate', 1);
   // Create a new transaction and specify the database and data privileges.
   const tx = jateDb.transaction('jate', 'readwrite');
   // Open up the desired object store.
@@ -38,8 +38,8 @@ export const getDb = async () => {
   const request = store.getAll();
   // Get confirmation of the request.
   const result = await request;
-  console.log('Database results: ', result);
-  return result.value;
+  console.log('result.value ', result.value);
+  return result?.value;
 }
 // console.error('getDb not implemented');
 
